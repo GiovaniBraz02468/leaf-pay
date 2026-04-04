d1cb7ca00a35e465179651e3b25e75488085314d
Giovani Rafael Tavares Braz <giovanibraz02468@gmail.com>
2026-04-04 12:32:51 -0300

    feat: implementação de multi-linguagem e alias
    Implementação de:
    
    Multi-linguagem: biblioteca e configuração para arquivos gerais
    alias: utilizar @ para poder ter caminho relativo

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
+import '@/shared/lib/i18n.ts';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
