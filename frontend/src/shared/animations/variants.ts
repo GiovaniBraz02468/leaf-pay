627d80935d18beebd6788e414113aa9ff074ec72
Jaca <eduardo.costa.miguel@escola.pr.gov.br>
2026-04-12 10:12:08 -0300

    feat: Criando arquivo base das animações com frame-motion
    Criando o arquivo base das animações

---
+++ frontend/src/shared/animations/variants.ts
@@ -0,0 +1,19 @@
+export const ANIM_VARIANTS_SYSTEM = {
+  buttons: {
+    hidden: {},
+    animate: {},
+    exit: {},
+  },
+
+  openModals: {
+    hidden: {},
+    animate: {},
+    exit: {},
+  },
+
+  closeModals: {
+    hidden: {},
+    animate: {},
+    exit: {},
+  },
+};
