163b874c3d8b8db5042ac9fb138808e21ad8dbe1
Everton Carlos <Evertonlensson@hotmail.com>
2026-04-14 19:59:49 -0300

    feat(auth): login page structure with layout and form

---
+++ frontend/src/features/auth/components/LoginForm.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+
+export const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log({ email, password });
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="bg-white p-8 rounded-2xl shadow-md w-full max-w-md"
+    >
+      <h2 className="text-2xl font-bold text-center mb-6 text-[var(--color-dark)]">
+        Entrar
+      </h2>
+
+      <div className="flex flex-col gap-4">
+        <input
+          type="email"
+          placeholder="Email"
+          className="border p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-[var(--color-primary)]"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Senha"
+          className="border p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-[var(--color-primary)]"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button
+          type="submit"
+          className="bg-[var(--color-primary)] text-white p-3 rounded-lg font-semibold hover:opacity-90 transition"
+        >
+          Entrar
+        </button>
+      </div>
+    </form>
+  );
+};
---
+++ frontend/src/features/auth/pages/loginPage.tsx
@@ -0,0 +1,22 @@
+import { LoginForm } from '../components/loginForm';
+// import logo from '@/assets/logo/logo.png';
+
+export const LoginPage = () => {
+  return (
+    <div className="min-h-screen grid grid-cols-1 md:grid-cols-2">
+      {/* LADO ESQUERDO */}
+      <div className="hidden md:flex flex-col justify-center items-center bg-[var(--color-primary)] text-white p-8">
+        <h1 className="text-3xl font-bold">LeafPay</h1>
+
+        <p className="text-sm mt-2 text-center max-w-xs">
+          Organize sua vida financeira de forma simples
+        </p>
+      </div>
+
+      {/* LADO DIREITO */}
+      <div className="flex items-center justify-center bg-[var(--color-background)] p-6">
+        <LoginForm />
+      </div>
+    </div>
+  );
+};
