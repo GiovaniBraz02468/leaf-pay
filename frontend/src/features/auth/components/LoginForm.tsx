1bec527691c21af36094a1041046e76ff01f8df3
giovanaassis <giovana_2011_11@hotmail.com>
2026-04-14 21:08:41 -0300

    feat: add project fonts and refactor tailwind styles

--- frontend/src/features/auth/components/LoginForm.tsx
+++ frontend/src/features/auth/components/LoginForm.tsx
@@ -14,7 +14,7 @@ export const LoginForm = () => {
       onSubmit={handleSubmit}
       className="bg-white p-8 rounded-2xl shadow-md w-full max-w-md"
     >
-      <h2 className="text-2xl font-bold text-center mb-6 text-[var(--color-dark)]">
+      <h2 className="text-2xl font-bold text-center mb-6 text-dark">
         Entrar
       </h2>
 
@@ -22,7 +22,7 @@ export const LoginForm = () => {
         <input
           type="email"
           placeholder="Email"
-          className="border p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-[var(--color-primary)]"
+          className="border p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
@@ -30,14 +30,14 @@ export const LoginForm = () => {
         <input
           type="password"
           placeholder="Senha"
-          className="border p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-[var(--color-primary)]"
+          className="border p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
 
         <button
           type="submit"
-          className="bg-[var(--color-primary)] text-white p-3 rounded-lg font-semibold hover:opacity-90 transition"
+          className="bg-primary text-white p-3 rounded-lg font-semibold hover:opacity-90 transition"
         >
           Entrar
         </button>
--- frontend/src/features/auth/pages/loginPage.tsx
+++ frontend/src/features/auth/pages/loginPage.tsx
@@ -1,12 +1,12 @@
-import { LoginForm } from '../components/loginForm';
+import { LoginForm } from "../components/LoginForm";
 // import logo from '@/assets/logo/logo.png';
 
 export const LoginPage = () => {
   return (
     <div className="min-h-screen grid grid-cols-1 md:grid-cols-2">
       {/* LADO ESQUERDO */}
-      <div className="hidden md:flex flex-col justify-center items-center bg-[var(--color-primary)] text-white p-8">
-        <h1 className="text-3xl font-bold">LeafPay</h1>
+      <div className="hidden md:flex flex-col justify-center items-center bg-primary text-white p-8">
+        <h1 className="text-3xl font-bold font-title">LeafPay</h1>
 
         <p className="text-sm mt-2 text-center max-w-xs">
           Organize sua vida financeira de forma simples
@@ -14,7 +14,7 @@ export const LoginPage = () => {
       </div>
 
       {/* LADO DIREITO */}
-      <div className="flex items-center justify-center bg-[var(--color-background)] p-6">
+      <div className="flex items-center justify-center bg-background p-6">
         <LoginForm />
       </div>
     </div>
