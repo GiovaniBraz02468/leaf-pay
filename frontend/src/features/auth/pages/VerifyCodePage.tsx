4f7646b2bca88bf4fcd2513c5bd0e35b3f9d03be
giovanaassis <giovana_2011_11@hotmail.com>
2026-04-24 00:32:15 -0300

    feat: add ui component to verify email code

--- frontend/src/features/auth/pages/VerifyCodePage.tsx
+++ frontend/src/features/auth/pages/VerifyCodePage.tsx
@@ -1,16 +1,29 @@
 import { FaCheckCircle } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { REGEXP_ONLY_DIGITS } from "input-otp";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
 
 export default function VerifyCodePage() {
   return (
-    <div className="flex flex-col text-center gap-5">
+    <div className="flex flex-col items-center text-center gap-5">
       <div className="flex gap-1">
         <FaCheckCircle className="text-primary text-2xl" />
         <p>Enviamos um código para seu e-mail para redefinir sua senha.</p>
       </div>
 
       <p>Insira o código de verificação</p>
-      {/* TODO -> CRIAR O MODAL DO CÓDIGO */}
+      <InputOTP maxLength={4} pattern={REGEXP_ONLY_DIGITS} className="" >
+        <InputOTPGroup>
+          <InputOTPSlot index={0} />
+          <InputOTPSlot index={1} />
+          <InputOTPSlot index={2} />
+          <InputOTPSlot index={3} />
+        </InputOTPGroup>
+      </InputOTP>
 
       <small>
         Não recebeu o código? <span className="text-primary">Reenvie.</span>
